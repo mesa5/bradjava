a2c98e714a203fdd5874b8a8a123f4fa2aafe081
Brad Chao <brad@brad.tw>
2016-09-04 14:26:54 +0800

    TCP v3

--- src/tw/brad/bradjava/TCPClient.java
+++ src/tw/brad/bradjava/TCPClient.java
@@ -2,36 +2,37 @@ package tw.brad.bradjava;
 
 import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
+import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.OutputStream;
 import java.net.InetAddress;
 import java.net.Socket;
+
+import com.sun.security.ntlm.Client;
 
 public class TCPClient {
 	public static void main(String[] args) {
 		// 1. read from local filesystem
 		long start = System.currentTimeMillis();
 		try {
-			Socket client = 
-				new Socket(InetAddress.getByName("10.2.24.119"), 
-						6666);
-			BufferedOutputStream bout = 
-				new BufferedOutputStream(client.getOutputStream());
+			File sendFile = new File("dir1/coffee.jpg");
+			byte[] sendBuf = new byte[(int)sendFile.length()];
 			BufferedInputStream bin = 
 				new BufferedInputStream(
-					new FileInputStream("dir1/coffee.jpg"));
-			int b;
-			while ( (b = bin.read()) != -1){
-				bout.write(b);
-			}
-			
+					new FileInputStream(sendFile));
+			bin.read(sendBuf);
 			bin.close();
+			
+			// 2. send by TCP
+			Socket client = 
+				new Socket(InetAddress.getByName("10.2.24.119"), 
+				6666);
+			BufferedOutputStream bout = 
+				new BufferedOutputStream(client.getOutputStream());
+			bout.write(sendBuf);
 			bout.flush();
 			bout.close();
 			client.close();
-			System.out.println(System.currentTimeMillis() - start);
+			System.out.println(System.currentTimeMillis()-start);
 			System.out.println("Send OK");
 		} catch (Exception e) {
 			System.out.println(e.toString());
