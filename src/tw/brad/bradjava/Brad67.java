8e96ccfa5f9d635dbbdb9ab674ba6223dc8c034e
Brad Chao <brad@brad.tw>
2016-09-11 11:29:07 +0800

    Brad66 v4

---
+++ src/tw/brad/bradjava/Brad67.java
@@ -0,0 +1,18 @@
+package tw.brad.bradjava;
+
+import java.util.HashMap;
+
+import com.sun.xml.internal.ws.api.ha.HaInfo;
+
+public class Brad67 {
+
+	public static void main(String[] args) {
+		HashMap<Integer,String> map = 
+			new HashMap<>();
+		map.put(0, "brad");
+		map.put(1, "3");
+		map.put(7, "male");
+		System.out.println(map.get(7));
+	}
+
+}
--- src/tw/brad/bradjava/MyPainter.java
+++ src/tw/brad/bradjava/MyPainter.java
@@ -1,37 +1,61 @@
 package tw.brad.bradjava;
 
+import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
-import java.awt.event.MouseMotionListener;
+import java.util.HashMap;
+import java.util.LinkedList;
 
 import javax.swing.JPanel;
 
 public class MyPainter extends JPanel {
-	public MyPainter(){
+	private LinkedList<HashMap<String, Integer>> line;
+
+	public MyPainter() {
 		MyAdapter adapter = new MyAdapter();
 		addMouseListener(adapter);
 		addMouseMotionListener(adapter);
+		line = new LinkedList<>();
 	}
+
 	@Override
 	protected void paintComponent(Graphics g) {
 		super.paintComponent(g);
-		Graphics2D g2d = (Graphics2D)g;
-		
+		Graphics2D g2d = (Graphics2D) g;
+
 		g2d.setColor(Color.BLUE);
+		g2d.setStroke(new BasicStroke(4));
+		for (int i=1; i<line.size(); i++){
+			HashMap<String,Integer> p0 = line.get(i-1);
+			HashMap<String,Integer> p1 = line.get(i);
+			int p0x = p0.get("x"), p0y = p0.get("y");
+			int p1x = p1.get("x"), p1y = p1.get("y");
+			g2d.drawLine(p0x, p0y, p1x, p1y);
+		}
 	}
+
 	private class MyAdapter extends MouseAdapter {
 		@Override
 		public void mouseDragged(MouseEvent e) {
 			super.mouseDragged(e);
+			addPoint(e);
+			repaint();
 		}
+
 		@Override
 		public void mousePressed(MouseEvent e) {
 			super.mousePressed(e);
+			addPoint(e);
+		}
+		private void addPoint(MouseEvent e){
+			HashMap<String, Integer> point = new HashMap<>();
+			point.put("x", e.getX());
+			point.put("y", e.getY());
+			line.add(point);
 		}
 	}
-	
+
 }
