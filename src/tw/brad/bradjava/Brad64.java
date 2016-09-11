d0bd57154d8c2c9dd7ba861a11505e5c0c7bb923
Brad Chao <brad@brad.tw>
2016-09-11 09:54:06 +0800

    Brad64, 65

---
+++ src/tw/brad/bradjava/Brad64.java
@@ -0,0 +1,23 @@
+package tw.brad.bradjava;
+
+public class Brad64 {
+
+	public static void main(String[] args) {
+
+	}
+
+}
+interface Brad641 {
+	void m1();
+}
+interface Brad642{
+	void m2();
+}
+interface Brad643 extends Brad641, Brad642 {
+	void m3();
+}
+class Brad644 implements Brad643 {
+	public void m1(){}
+	public void m2(){}
+	public void m3(){}
+}
